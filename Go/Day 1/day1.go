795c52bdde4fe62bffcc7fc00374c4cb442aec02
Maitgon <aitor98gfgb@gmail.com>
2021-12-01 16:45:08 +0100

    Benchmark added

--- Go/Day 1/day1.go
+++ Go/Day 1/day1.go
@@ -7,9 +7,12 @@ import (
 	"io/ioutil"
 	"strings"
 	"strconv"
+	"time"
 )
 
 func main() {
+
+	start := time.Now()
 
 	// Input reading
 	bs, err := ioutil.ReadFile("./Go/Day 1/input.txt")
@@ -20,32 +23,36 @@ func main() {
 
 	input := strings.Split(string(bs), "\n")
 
-	var vals []int64
+	var vals []int16
 	for _, value := range input {
 		val, _ := strconv.ParseInt(value, 10, 16)
-		vals = append(vals, val)
+		vals = append(vals, int16(val))
 	}
 	values := vals[:len(vals)-1]
 
 	sol1 := part1(values)
-	fmt.Println("The solution to part 1 is: ", sol1)
 
 	sol2 := part2(values)
+
+	end := time.Since(start)
+
+	fmt.Println("The solution to part 1 is: ", sol1)
 	fmt.Println("The solution to part 2 is: ", sol2)
+	fmt.Println("Time: ", end)
 
 }
 
-func part1(values []int64) int64 {
+func part1(values []int16) int16 {
 	count := 0
 	for i := 0; i < len(values) - 1; i++ {
 		if values[i] < values[i+1] {
 			count++
 		}
 	}
-	return int64(count)
+	return int16(count)
 }
 
-func part2(values []int64) int64 {
+func part2(values []int16) int16 {
 	count := 0
 	for i := 0; i < len(values) - 3; i++ {
 		if values[i] + values[i+1] + values[i+2] <
@@ -53,5 +60,5 @@ func part2(values []int64) int64 {
 			   count++
 		   }
 	}
-	return int64(count)
+	return int16(count)
 }
--- Readme.md
+++ Readme.md
@@ -1,3 +1,9 @@
 # Advent of code 2021
 
 These are my solutions for the advent of code 2021 event. My idea is to solve all the problems in go, but I may use another language if I get stuck. Wish everyone a happy advent 2021 :)
+
+## Benchmarks
+
+| Days | Time |
+| :----: | :----: |
+| Day 1 | 302&mu;s |
